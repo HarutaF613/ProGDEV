7d150af5dd60d0a986614dbe301a8c6aa59d0096
HarutaF613 <harutan5555@gmail.com>
2025-08-28 18:59:07 +0200

    Add files via upload

--- 2025-08-28-Lesson6-SpaceInvader/cw.py
+++ 2025-08-28-Lesson6-SpaceInvader/cw.py
@@ -24,6 +24,11 @@ keys1 = [False for i in range(4)]
 #JILK
 keys2 = [False for i in range(4)]
 
+b1 = pygame.transform.scale(pygame.image.load("bullet.png"),(50,50))
+b1_y = 0
+b2 = pygame.transform.scale(pygame.transform.scale(pygame.image.load("bullet.png"),(50,50)),90)
+b2_y = 0
+
 while True:
     display.blit(bg,(0,0))
     display.blit(ship1,(s1_x,s1_y))
@@ -35,62 +40,71 @@ while True:
             exit(0)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                keys1[0] == True
-            if event.key == pygame.K_w:
-                keys1[1] == True
-            if event.key == pygame.K_d:
-                keys1[2] == True
-            if event.key == pygame.K_s:
-                keys1[3] == True
+                keys1[0] = True
+            elif event.key == pygame.K_w:
+                keys1[1] = True
+            elif event.key == pygame.K_d:
+                keys1[2] = True
+            elif event.key == pygame.K_s:
+                keys1[3] = True
             #
             if event.key == pygame.K_j:
-                keys2[0] == True
-            if event.key == pygame.K_i:
-                keys2[1] == True
-            if event.key == pygame.K_l:
-                keys2[2] == True
-            if event.key == pygame.K_k:
-                keys2[3] == True
+                keys2[0] = True
+            elif event.key == pygame.K_i:
+                keys2[1] = True
+            elif event.key == pygame.K_l:
+                keys2[2] = True
+            elif event.key == pygame.K_k:
+                keys2[3] = True
         #
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
-                keys1[0] == False
+                keys1[0] = False
             if event.key == pygame.K_w:
-                keys1[1] == False
+                keys1[1] = False
             if event.key == pygame.K_d:
-                keys1[2] == False
+                keys1[2] = False
             if event.key == pygame.K_s:
-                keys1[3] == False
+                keys1[3] = False
             #
             if event.key == pygame.K_j:
-                keys2[0] == False
+                keys2[0] = False
             if event.key == pygame.K_i:
-                keys2[1] == False
+                keys2[1] = False
             if event.key == pygame.K_l:
-                keys2[2] == False
+                keys2[2] = False
             if event.key == pygame.K_k:
-                keys2[3] == False
+                keys2[3] = False
+    #bullet1
+    if b1_y > -100:
+        b1_y -= 0.5
+    display.blit(b1,(s1_x,b1_y))
+    
     #Ship1
     if keys1[0] == True:
-         s1_x -= 5
+        if s1_x > 0:
+            s1_x -= 0.5
     if keys1[1] == True:
-        s1_y -= 5
+        if s1_y > 0:
+            s1_y -= 0.5
     if keys1[1] == False:
         if s1_y < HEIGHT-HEIGHT/8:
-            s1_y += 5
+            s1_y += 0.5
     if keys1[2] == True:
-        s1_x += 5
-    #if keys1[3] == True:
+        if s1_x < WIDTH-100:
+            s1_x += 0.5
+    if keys1[3] == True:
+        b1_y = s1_y
 
     #Ship2
     if keys2[0] == True:
-        s2_x -= 5
+        s2_x -= 0.5
     if keys2[1] == True:
-        s2_y += 5
+        s2_y += 0.5
     if keys2[1] == False:
         if s2_y > 0:
-            s2_y -= 5
+            s2_y -= 0.5
     if keys2[2] == True:
-        s2_x += 5
+        s2_x += 0.5
     #if keys2[3] == True:
     print("Keys1 : {}".format(keys1))
