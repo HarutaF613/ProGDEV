a5a6b495a59c3264505e446bde74bbd9300386a5
HarutaF613 <harutan5555@gmail.com>
2025-10-09 18:58:51 +0200

    Add files via upload

--- 2025-10-02-Lesson10-FlappyBird/@cw.py
+++ 2025-10-02-Lesson10-FlappyBird/@cw.py
@@ -29,15 +29,42 @@ class Bird(pygame.sprite.Sprite):
         self.image3 = pygame.image.load("fish3.png")
         self.images.append(self.image3)
 
-        self.vel = 0
+        self.vel = 1 #Velocity
+        #Velocity should increase by every obstacles
         self.click = False
         self.dead = False
     
-    def update(self):
-        if self.click == True:
+    #def update(self):
+        #if self.click == True:
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.self_img1 = pygame.image.load("pipe.png")
+        self.self_img2 = pygame.transform.rotate(self.self_img1,180)
+
+        #self.pos = 1
+        if self.pos == 1:
+
+        self.self_y = random.randint(200,HEIGHT-200)
+        self.self_x = 0
+
+        if pos == 1:
+            #Use image1
+            self.img = self.self_img1
+            
+        elif pos == -1:
+            self.img = self.self_img2
+
+sprites = pygame.sprite.Group()       
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+    sprites.draw()
+
+"""
+pipe1 : self_y + 50
+pipe2 : self_y - 50
+"""
